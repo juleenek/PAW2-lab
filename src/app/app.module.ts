61b42579302de0eb27f0e1d0e866bdeb81611a43
juleenek <leeenek0@gmail.com>
2023-03-16 18:18:29 +0100

    First column done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponentComponent } from './card-component/card-component.component';
 import { RevenueBoxComponent } from './revenue-box/revenue-box.component';
+import { MediumChartComponent } from './medium-chart/medium-chart.component';
+import { SalesCountryComponent } from './sales-country/sales-country.component';
+import { SalesCountryChartComponent } from './sales-country-chart/sales-country-chart.component';
+import { NetIncomeComponent } from './net-income/net-income.component';
+import { TopProductsComponent } from './top-products/top-products.component';
+import { ProductComponentComponent } from './product-component/product-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardComponentComponent,
-    RevenueBoxComponent
+    RevenueBoxComponent,
+    MediumChartComponent,
+    SalesCountryComponent,
+    SalesCountryChartComponent,
+    NetIncomeComponent,
+    TopProductsComponent,
+    ProductComponentComponent
   ],
   imports: [
     BrowserModule,
